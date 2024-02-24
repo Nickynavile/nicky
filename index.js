32b826edef1e7d02ae112dad3997ca87f01cdd23
Nickynavile <nickynavile@gmail.com>
2024-02-25 04:03:13 +0530

    First version

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+console.log(`
+Hello, I'm Nikhilesh Navile! 👋
+
+I am a Final year Student in Electronics and Communications Engineering @IIITDM Jabalpur Campus. Currently exploring Full Stack web Development. Always trying to learn new javascript libraries and frameworks.
+
+🪶  twitter: https://twitter.com/nickynavile
+💻 Github: https://github.com/nickynavile
+`)
