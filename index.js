bf9bb507c42649cb0d452b952aa3c1f24bee3dca
Nickynavile <nickynavile@gmail.com>
2024-02-25 06:22:04 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
 
+const pkgJson = require('./package.json')
+const welcome = require('cli-welcome')
+
+welcome({
+    title: pkgJson.name,
+    description: pkgJson.description,
+    version: pkgJson.version,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    clear: true
+})
+
+
 console.log(`
-Hello, I'm Nikhilesh Navile! 👋
+Hello,I'm Nikhilesh Navile! 👋
 
 I am a Final year Student in Electronics and Communications Engineering @IIITDM Jabalpur Campus. Currently exploring Full Stack web Development. Always trying to learn new javascript libraries and frameworks.
 
